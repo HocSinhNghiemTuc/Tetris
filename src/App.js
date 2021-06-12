1695d9e731d32f3362084fc65ae87b849856a326
dungbk10t <dungbk10t@gmail.com>
2021-06-12 20:48:29 +0700

    Update userprofile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
+import Login from "./components/Login";
+import { auth, storeUserInfo, updateUser } from "./lib/firebase";
 
 // import Tetris from "./components/Tetris";
-import Admin from "./components/Admin";
 import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import User from "./components/Admin/components/User";
 import Header from "./components/Admin/components/Header";
 import TableQL from "./components/Admin/components/TableQL";
+import Upload from "./components/Upload";
 
 export default function App() {
-  // return <Tetris />;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      setLoading(false);
+      let newUser = null;
+      if (user) {
+        newUser = await storeUserInfo(user);
+      }
+      setUser(newUser);
+    });
+  }, []);
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  const handleImageChanged = async downlodUrl => {
+    await updateUser(user, downlodUrl);
+  }
+
+  const HeaderContent = () => {
+    if (user) {
+      return (
+        <div class='navbar-end'>
+          <div class='navbar-item'>
+            <Upload userImage={user.image} onSelectImage={handleImageChanged} />
+            {user.name}
+          </div>
+          <div class='navbar-item'>
+            <button class='button is-danger is-light is-small' onClick={logout}>
+              {" "}
+              Logout
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return <Login />;
+    }
+  };
   return (
     <BrowserRouter>
-      <Header/>
-      <Switch>
-        <Route path="/" exact component={TableQL}/>
-        <Route path="/users" component={User}/>
-      </Switch>
-    </BrowserRouter>
-    
+    <div className='container is-fluid'>
+      <header class='navbar'>
+        {loading ? <p>LOADING.....</p> : <HeaderContent />}
+      </header>
+      <div>{user && <Header/>}</div>
+    </div>
+    <Switch>
+      <Route path='/' exact component={TableQL} />
+      <Route path='/users' component={User} />
+    </Switch>
+  </BrowserRouter>
   );
 }
---
+++ src/lib/firebase.js
@@ -0,0 +1,80 @@
+import firebase from 'firebase';
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAreiU_t9Q6GtpRTrgh-TQSbkZi4fPGsZw",
+    authDomain: "tetris-63be6.firebaseapp.com",
+    projectId: "tetris-63be6",
+    storageBucket: "tetris-63be6.appspot.com",
+    messagingSenderId: "221757421981",
+    appId: "1:221757421981:web:fe64f5b11d0820777f5774"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
